618ff20f720fddcf7077ec8b26c67b38662ef155
Valdir Lourenco <valdir2310@yahoo.com.br>
2020-02-28 17:45:00 -0300

    first simple example of get group

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SmartDog23\AzureFaceApi\AzureFaceApi;
+use SmartDog23\LaravelAzureFaceApi\Facades\LaravelAzureFaceApi;
 
 class GroupController extends Controller
 {
     public function __construct()
     {
-        
+
     }
 
     public function index()
     {
-        
+        $result = LaravelAzureFaceApi::largePersonGroup()->get()->execute('htest-25');
+        dump($result->getBody()->getContents());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/group/index', 'GroupController@index')->name('group-index');
